ba542791afdea6e5423b883cba784fcb9cbfdfb8
Carlos Landaeta <calejandrolg99@gmail.com>
2023-07-02 00:48:52 -0400

    Ya compila Client pero todavia hay errores

--- mini-banco/client/src/main/java/com/example/BankInterface.java
+++ mini-banco/client/src/main/java/com/example/BankInterface.java
@@ -1,10 +1,16 @@
+package java.com.example;
+
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface BankInterface extends Remote {
     String createAccount(String id, String name, String username, String password) throws RemoteException;
+
     String deposit(String accountNumber, double amount) throws RemoteException;
+
     String withdraw(String accountNumber, double amount) throws RemoteException;
+
     String transfer(String fromAccountNumber, String toAccountNumber, double amount) throws RemoteException;
+
     String getAccountDetails(String accountNumber) throws RemoteException;
 }
--- mini-banco/client/src/main/java/com/example/Client.java
+++ mini-banco/client/src/main/java/com/example/Client.java
@@ -1,3 +1,5 @@
+package java.com.example;
+
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
